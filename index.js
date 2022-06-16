6fc420b07e3f0b747cae1fc18e486ca3d51d74ea
Kunal Thite <thite2001@gmail.com>
2022-06-16 23:24:06 +0530

    second commit

--- index.js
+++ index.js
@@ -199,6 +199,10 @@ app.post("/login", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
   console.log("Server Started Succesfully");
 });
